c92f822be37a5a969649649bd7f369d00189e7a7
AllanJeremy <aj.dev254@gmail.com>
2022-01-15 17:49:22 +0300

    [Enhancement] Added logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-link :to="url"><img src="/logo.png" height="50" /></router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Logo',
+  props: {
+    url: { type: String, default: '/' },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
