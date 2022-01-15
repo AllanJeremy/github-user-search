33c8fdef3f5f85fd24cf3fafacdb09827d105f50
AllanJeremy <aj.dev254@gmail.com>
2022-01-15 17:57:12 +0300

    [Feature] Implemented search results page Displays search results found

--- src/pages/Results.vue
+++ src/pages/Results.vue
@@ -1,21 +1,54 @@
 <template>
-  <div>
-<h1>Search results</h1>
-  </div>
+  <!-- Under normal circumstances, this would be in the layout component -->
+  <q-header reveal>
+    <q-toolbar class="row bg-white shadow-4">
+      <Logo url="/" />
+      <q-space></q-space>
+      <SearchInput
+        class="q-ml-sm"
+        dense
+        max-width="20rem"
+        :default-value="searchText"
+        @search="updateSearchText"
+      />
+      <q-space></q-space>
+    </q-toolbar>
+  </q-header>
+
+  <q-page class="q-mx-auto q-my-lg q-pa-lg" style="max-width: 80rem">
+    <h5 class="text-weight-light q-my-none">
+      Search
+      <span class="text-weight-bold">{{
+        searchText ? `: ${searchText}` : ''
+      }}</span>
+    </h5>
+
+    <SearchResultList :search-text="searchText" />
+  </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+//* Components
+import Logo from 'src/components/Logo.vue';
+import SearchInput from 'src/components/inputs/SearchInput.vue';
+import SearchResultList from 'src/components/search/SearchResultList.vue';
 
 export default defineComponent({
-  setup () {
-    
+  components: { Logo, SearchInput, SearchResultList },
+  setup() {
+    const $route = useRoute();
+    const searchText = ref($route.params?.searchText?.toString());
 
-    return {}
-  }
-})
+    const updateSearchText = (newSearchText?: string) => {
+      searchText.value = newSearchText || $route.params?.searchText?.toString();
+    };
+
+    return { searchText, updateSearchText };
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
