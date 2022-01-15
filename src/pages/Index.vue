56e0f0bcab8c6264b84f562c580ce7960a193b93
AllanJeremy <aj.dev254@gmail.com>
2022-01-15 09:26:28 +0300

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="row items-center justify-evenly"> </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  setup() {
+    const searchText = ref('');
+
+    return { searchText };
+  },
+});
+</script>
---
+++ src/pages/Results.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+<h1>Search results</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
