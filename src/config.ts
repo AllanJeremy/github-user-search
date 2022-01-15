7201b0fceb82895257e8d849644548f6760a62ec
AllanJeremy <aj.dev254@gmail.com>
2022-01-15 17:45:38 +0300

    [Config] Added config file with github username & personal access token This should ideally be stored somewhere secure normally

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+//! Ideally I'd store this on the backend but for the sake of time, I am storing this on the client side 😅
+export const GITHUB_USERNAME = 'allanjeremy';
+
+export const GITHUB_PERSONAL_ACCESS_TOKEN =
+  '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';
